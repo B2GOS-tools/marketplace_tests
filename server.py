bd5d1daba5a48f78e9bf420578090bc27acb7d14
Malini Das <mdas@mozilla.com>
2014-04-14 20:08:58 -0400

    remove local server code and add logcat code

--- server.py
+++ server.py
@@ -6,27 +6,10 @@ import time
 from gaiatest import GaiaApps
 from marionette import Marionette
 from marionette.wait import Wait
+from mozdevice import DeviceManagerADB
 from mozhttpd import MozHttpd
 import moznetwork
 
-
-def start_httpd(need_external_ip):
-    host = "127.0.0.1"
-    if need_external_ip:
-        host = moznetwork.get_ip()
-    docroot = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'apps')
-    if not os.path.isdir(docroot):
-        raise Exception("Server root %s is not a valid path" % docroot)
-    print docroot
-    httpd = MozHttpd(host=host,
-                          port=0,
-                          docroot=docroot)
-    httpd.start()
-    return (httpd, host)
-
-httpd, host = start_httpd(True)
-baseurl = 'http://%s:%d/' % (host, httpd.httpd.server_port)
-print baseurl
 apps = None
 with open('manifest', 'r') as f:
     apps = json.loads(f.read())
@@ -37,7 +20,7 @@ gaia_apps = GaiaApps(m)
 gaia_apps.kill_all()
 
 install_script = """
-var installUrl = '%s%s';
+var installUrl = '%s';
 var request = window.navigator.mozApps.%s(installUrl);
 request.onsuccess = function () {
   window.wrappedJSObject.marionette_install_result = true;
@@ -46,17 +29,24 @@ request.onerror = function () {
  window.wrappedJSObject.marionette_install_result = this.error.name;
 };
 """
+# TODO: assumes adb is on path
+dm = DeviceManagerADB()
+if not os.path.exists("logcats"):
+    os.makedirs("logcats")
+logcat = dm.getLogcat()
+with open("logcats/before_test.logcat", "w") as f:
+    for line in logcat:
+        f.write(line)
 for app_name, path in apps.iteritems():
+    # clear logcat
+    dm._checkCmd(["logcat", "-c"])
     # launch (or switch to) marketplace, wait 3 minutes for successful launch
     marketplace_app = gaia_apps.launch("Marketplace", switch_to_frame=True, launch_timeout=180000)
     # trigger the install
-    print "%s%s" % (baseurl, path)
-    #TODO: remove
-    baseurl = ''
     if path.endswith(".webapp"):
-        m.execute_script(install_script % (baseurl, path, "install"), script_timeout=30000)
+        m.execute_script(install_script % (path, "install"), script_timeout=30000)
     elif path.endswith(".zip"):
-        m.execute_script(install_script % (baseurl, path, "installPackage"), script_timeout=30000)
+        m.execute_script(install_script % (path, "installPackage"), script_timeout=30000)
     else:
         raise Exception("Can't install from given path: %s" % path)
     # go to system app
@@ -75,11 +65,13 @@ for app_name, path in apps.iteritems():
     Wait(m, timeout=180).until(lambda m: m.execute_script("return window.document.readyState;") == "complete")
     shot = m.screenshot()
     img = base64.b64decode(shot.encode('ascii'))
-    with open("%s_screenshot.png" % app_name, "w") as f:
+    if not os.path.exists("screenshots"):
+        os.makedirs("screenshots")
+    with open("screenshots/%s.png" % app_name, "w") as f:
         f.write(img)
     gaia_apps.kill_all()
     gaia_apps.uninstall(app_name)
-    import pdb;pdb.set_trace()
-    # TODO: get logcats
-
-httpd.stop()
+    logcat = dm.getLogcat()
+    with open("logcats/%s.logcat" % app_name, "w") as f:
+        for line in logcat:
+            f.write(line)
