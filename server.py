ca47a29e52711c8d05c278ae7252877eeadbce84
Malini Das <mdas@mozilla.com>
2014-04-14 20:15:05 -0400

    add logcat saving

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import time
 
 from gaiatest import GaiaApps
 from marionette import Marionette
+from marionette.wait import Wait
+from mozdevice import DeviceManagerADB
 from mozhttpd import MozHttpd
 import moznetwork
 
@@ -33,7 +35,6 @@ with open('manifest', 'r') as f:
 m = Marionette()
 m.start_session()
 gaia_apps = GaiaApps(m)
-import pdb;pdb.set_trace()
 gaia_apps.kill_all()
 
 install_script = """
@@ -46,7 +47,16 @@ request.onerror = function () {
  window.wrappedJSObject.marionette_install_result = this.error.name;
 };
 """
+dm = DeviceManagerADB()
+if not os.path.exists("logcats"):
+    os.makedirs("logcats")
+logcat = dm.getLogcat()
+with open("logcats/before_test.logcat", "w") as f:
+    for line in logcat:
+        f.write(line)
 for app_name, path in apps.iteritems():
+    # clear logcat
+    dm._checkCmd(["logcat", "-c"])
     # launch (or switch to) marketplace, wait 3 minutes for successful launch
     marketplace_app = gaia_apps.launch("Marketplace", switch_to_frame=True, launch_timeout=180000)
     # trigger the install
@@ -64,12 +74,19 @@ for app_name, path in apps.iteritems():
     assert m.execute_script("return window.wrappedJSObject.marionette_install_result")
     # launch app, wait 3 minutes
     app_under_test = gaia_apps.launch(app_name, switch_to_frame=True, launch_timeout=180000)
+    # Wait at most 3 minutes for app to load
+    Wait(m, timeout=180).until(lambda m: m.execute_script("return window.document.readyState;") == "complete")
     shot = m.screenshot()
     img = base64.b64decode(shot.encode('ascii'))
-    with open("%s_screenshot.png" % app_name, "w") as f:
+    if not os.path.exists("screenshots"):
+        os.makedirs("screenshots")
+    with open("screenshots/%s.png" % app_name, "w") as f:
         f.write(img)
     gaia_apps.kill_all()
     gaia_apps.uninstall(app_name)
-    # TODO: get logcats
+    logcat = dm.getLogcat()
+    with open("logcats/%s.logcat" % app_name, "w") as f:
+        for line in logcat:
+            f.write(line)
 
 httpd.stop()
