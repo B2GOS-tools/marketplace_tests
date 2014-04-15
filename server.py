d8f62d8de686f011c3ab08176ca5d2a238986170
Malini Das <mdas@mozilla.com>
2014-04-15 12:00:26 -0400

    get content-type working

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import base64
 import json
 import os
-import time
+import StringIO
+import zipfile
 
 from gaiatest import GaiaApps
 from marionette import Marionette
@@ -11,6 +12,25 @@ from mozhttpd import MozHttpd
 import moznetwork
 
 
+def handler(req, ext):
+    filepath = req.path[1:] # remove prefixed '/'
+    path = os.path.join("apps", filepath)
+    if ext == "webapp":
+        f = open(path, "r")
+        data = f.read()
+        f.close()
+        return (200, {"Content-Type": "application/x-web-app-manifest+json", "Content-Length": len(data)}, data)
+    else:
+        #TODO: There has to be a better way to do this
+        data = StringIO.StringIO()
+        z_files = zipfile.ZipFile(path, "r")
+        z_out = zipfile.ZipFile(data, "w")
+        for filename in z_files.namelist():
+            z_out.writestr(filename, z_files.read(filename))
+        z_out.close()
+        z_files.close()
+        return (200, {"Content-Type": "application/x-web-app-manifest+json", "Content-Length": len(data.getvalue())}, data.getvalue())
+
 def start_httpd(need_external_ip):
     host = "127.0.0.1"
     if need_external_ip:
@@ -18,10 +38,11 @@ def start_httpd(need_external_ip):
     docroot = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'apps')
     if not os.path.isdir(docroot):
         raise Exception("Server root %s is not a valid path" % docroot)
-    print docroot
+    urlhandlers = [{'method':'GET', 'path':'.*\.(webapp|zip)$', 'function':handler}]
     httpd = MozHttpd(host=host,
                           port=0,
-                          docroot=docroot)
+                          docroot=docroot,
+                          urlhandlers=urlhandlers)
     httpd.start()
     return (httpd, host)
 
@@ -39,7 +60,7 @@ gaia_apps.kill_all()
 
 install_script = """
 var manifestUrl = '%s%s';
-var request = window.navigator.mozApps.install(manifestUrl);
+var request = window.navigator.mozApps.%s(manifestUrl);
 request.onsuccess = function () {
   window.wrappedJSObject.marionette_install_result = true;
 };
@@ -61,7 +82,10 @@ for app_name, path in apps.iteritems():
     marketplace_app = gaia_apps.launch("Marketplace", switch_to_frame=True, launch_timeout=180000)
     # trigger the install
     print "%s%s" % (baseurl, path)
-    m.execute_script(install_script % (baseurl, path), script_timeout=30000)
+    if path.endswith(".webapp"):
+        m.execute_script(install_script % (baseurl, path, "install"), script_timeout=30000)
+    elif path.endswith(".zip"):
+        m.execute_script(install_script % (baseurl, path, "installPackage"), script_timeout=30000)
     # go to system app
     m.switch_to_frame()
     # click Install
